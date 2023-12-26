0f89118bd90fa7aade4217743168c49170d2db5b
Rafffan <vanidrisraffan@gmail.com>
2023-12-26 16:58:24 +0700

    Lab11_.cpp compleated

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,12 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+
+
+}
+
